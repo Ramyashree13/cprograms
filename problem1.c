e6f2b6687ba247606b87d4c1e98a9d2e5b808d99
unknown <ramya>
2019-06-27 08:50:53 +0530

    compute function is in the process of constructing.

--- problem1.c
+++ problem1.c
@@ -1,16 +1,43 @@
 #include<stdio.h>
-struct rectangle
+#include<math.h>
+struct coordinates
 {
-	float x;
-	float y;
+	float x1;
+	float y1;
+	float x2;
+	float y2;
+	float x3;
+	float y3;
 };
 void input(int n,struct rectangle a[n])
 {
 	printf("enter the coordinates of rectangles\n");
 	for(int i=0;i<n;i++)
-		scanf("%f%f",&a[i].x,&a[i].y);
+		scanf("%f%f%f%f%f%f",&a[i].x1,&a[i].y1,&a[i].x2,&a[i].y2,&a[i].x3,&a[i].y3);
 	for(int i=0;i<n;i++)
-		printf("%f%f",a[i].x,a[i].y);
+		printf("%f%f",a[i].x1,a[i].y1,a[i].x2,a[i].y2,a[i].x3,a[i].y3);
+}
+void compute(int n,struct rectangle a[n])
+{
+
+	int len1[n],len2[n],len3[n],len4[n],p1[n],p2[n];
+	for(int i=0;i<n;i++)
+		len1[i]= (a[i].x2) - (a[i].x1);
+	for(int i=0;i<n;i++)
+		len2[i]= (a[i].x3) - (a[i].x2);
+	
+	for(int i=0;i<n;i++)
+		len3[i]=(a[i].y2) - (a[i].y1);
+	for(int i=0;i<n;i++)
+		len4[i]=(a[i].y3) - (a[i].y2);
+	for(i=0;i<n;i++)
+		p1[i]=sqrt((len1[i] * len1[i]) + (len3[i] * len3[i]));
+	for(i=0;i<n;i++)
+		p2[i]=sqrt((len2[i] * len2[i]) + (len4[i] * len4[i]));
+
+
+
+
 }
 int main()
 {
@@ -21,4 +48,5 @@ int main()
 	input(n,a);
 	return 0;
 }
+
 
