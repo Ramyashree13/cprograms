128cc39634b3bb444a2499c9fcb1f4d1dd3599e0
unknown <ramya>
2019-06-27 10:11:43 +0530

    output function is added.

--- problem1.c
+++ problem1.c
@@ -24,10 +24,11 @@ void input(int n,struct rectangle a[n])
 	for(int i=0;i<n;i++)
 		printf("%f\n%f\n%f\n%f\n%f\n%f\n",a[i].p.x1,a[i].p.y1,a[i].p.x2,a[i].p.y2,a[i].p.x3,a[i].p.y3);
 }
-void compute(int n,struct rectangle a[n])
+void compute(int n,struct rectangle a[n],float *res)
 {
 
-	float len1[n],len2[n],len3[n],len4[n],p1[n],p2[n],area[n];
+	float len1[n],len2[n],len3[n],len4[n],p1[n],p2[n];
+
 	for(int i=0;i<n;i++)
 		len1[i]= (a[i].p.x2) - (a[i].p.x1);
 	for(int i=0;i<n;i++)
@@ -42,9 +43,16 @@ void compute(int n,struct rectangle a[n])
 	for(int i=0;i<n;i++)
 		p2[i]=sqrt((len2[i] * len2[i]) + (len4[i] * len4[i]));
 	for(int i=0;i<n;i++)
-		area[i]= p1[i] * p2[i];
+		res[i]= p1[i] * p2[i];
 	for(int i=0;i<n;i++)
-		printf("%f",area[i]);
+		printf("%f",res[i]);
+}
+void output(int n,float area[])
+{
+	printf("the area of the %d rectangles are:\n",n);
+	for(int i=0;i<n;i++)
+		printf("%f\n",area[i]);
+
 }
 int main()
 {
@@ -52,8 +60,11 @@ int main()
 	printf("enter the number of rectangles you need to claculate the area\n");
 	scanf("%d",&n);
 	struct rectangle a[n];
+	float area[n],*res;
+	res=area;
 	input(n,a);
-	compute(n,a);
+	compute(n,a,res);
+	output(n,area);
 	return 0;
 }
 
