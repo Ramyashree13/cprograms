aa801a53b50fefa8eb06551d86129d3e146f1dd5
unknown <ramya>
2019-06-28 20:12:15 +0530

    when we need to send the pointers in reference. while declaring don't declare as the pointer.

--- problem1a.c
+++ problem1a.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-void smallest(float len1,float len2,float len3,float *smallest1,float *smallest2);
+int smallest(float len1,float len2,float len3,float *smallest1,float *smallest2);
 struct point
 {
 	float x;
@@ -32,7 +32,7 @@ void compute(int n,struct rectangle a[n],float *res)
 {
 
 	float len1,len2,len3,p1,p2,p3,p4,p5,p6;
-	float *smallest1=0,*smallest2=0;
+	float smallest1=0,smallest2=0;
 	for(int i=0;i<n;i++)
 	{
 		p1= (a[i].p[2].x) - (a[i].p[1].x);
@@ -44,32 +44,40 @@ void compute(int n,struct rectangle a[n],float *res)
 		len1=sqrt((p1 * p1) + (p4 * p4));
 		len2=sqrt((p2 * p2) + (p5 * p5));
 		len3=sqrt((p3 * p3) + (p6 * p6));
-		printf("%f\n%f\n%f\n",len1,len2,len3);
-		smallest(len1,len2,len3,smallest1,smallest2);
-		res[i] = (*smallest1) * (*smallest2);
+//		printf("%f\n%f\n%f\n",len1,len2,len3);
+	       	smallest(len1,len2,len3,&smallest1,&smallest2);
+		printf("rectangles\n");
+		printf("in compute:%f\n%f\n",smallest1,smallest2);
+		res[i] = (smallest1) * (smallest2);
 	}
-	printf("rectangles\n");
+	for(int i=0;i<n;i++)
+		printf("%f\n",res[i]);
+
 }
-void smallest(float len1,float len2,float len3,float *smallest1,float *smallest2)
+int smallest(float len1,float len2,float len3,float *smallest1,float *smallest2)
 {
 	if(len1>len2 && len1>len3)
 	{
 		*smallest1=len2;
 		*smallest2=len3;
-		printf("%f\n%f\n",*smallest1,*smallest2);
+		printf("in function %f\n%f\n",*smallest1,*smallest2);
+		return 0;
 	}
 	else if(len2>len1 && len2>len3)
 	{
 		*smallest1=len1;
 		*smallest2=len3;
-		printf("%f\n%f\n",*smallest1,*smallest2);
+		printf("in function %f\n%f\n",*smallest1,*smallest2);
+		return 0;
 	}
 	else 
 	{
 		*smallest1=len1;
 		*smallest2=len2;
-		printf("%f\n%f\n",*smallest1,*smallest2);
+		printf("in function %f\n%f\n",*smallest1,*smallest2);
+		return 0;
 	}
+	
 }
 
 void output(int n,float area[])
