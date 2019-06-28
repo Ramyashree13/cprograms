8d66da85ddaed3ec87385cbf177fec6263caeb4f
unknown <ramya>
2019-06-28 18:49:12 +0530

    smallest function has been added

--- problem1.c
+++ problem1.c
@@ -30,26 +30,66 @@ void input(int n,struct rectangle a[n])
 void compute(int n,struct rectangle a[n],float *res)
 {
 
-	float len1[n],len2[n],len3[n],len4[n],p1[n],p2[n];
-
+	float len1,len2,len3,len4,p1,p2,p3,p4,p5,p6;
+	float *smallest1,*smallest2;
+/*
 	for(int i=0;i<n;i++)
 		len1[i]= (a[i].p[2].x) - (a[i].p[1].x);
 	for(int i=0;i<n;i++)
 		len2[i]= (a[i].p[3].x) - (a[i].p[2].x);
+	for(int i=0;i<n;i++)
+		len5[i]= (a[i].p[3].x) - (a[i].p[1].x);
 	
 	for(int i=0;i<n;i++)
 		len3[i]=(a[i].p[2].y) - (a[i].p[1].y);
 	for(int i=0;i<n;i++)
 		len4[i]=(a[i].p[3].y) - (a[i].p[2].y);
 	for(int i=0;i<n;i++)
+		len6[i]=(a[i].p[3].y) - (a[i].p[1].y);
+	for(int i=0;i<n;i++)
 		p1[i]=sqrt((len1[i] * len1[i]) + (len3[i] * len3[i]));
 	for(int i=0;i<n;i++)
 		p2[i]=sqrt((len2[i] * len2[i]) + (len4[i] * len4[i]));
 	for(int i=0;i<n;i++)
-		res[i]= p1[i] * p2[i];
+		p3[i]=sqrt((len5[i] * len5[i]) + (len6[i] * len6[i]));
+
+	less=smallest(n,p1,p2,p3);
 	for(int i=0;i<n;i++)
-		printf("%f",res[i]);
+		res[i]= p1[i] * p2[i];*/
+	for(i=0;i<n;i++)
+	{
+		p1= (a[i].p[2].x) - (a[i].p[1].x);
+		p2= (a[i].p[3].x) - (a[i].p[2].x);
+		p3= (a[i].p[3].x) - (a[i].p[1].x);
+		p4=(a[i].p[2].y)  - (a[i].p[1].y);
+		p5=(a[i].p[3].y) - (a[i].p[2].y);
+		p6=(a[i].p[3].y) - (a[i].p[1].y);
+		len1=sqrt((p1 * p1) + (p4 * p4);
+		len2=sqrt((p2 * p2) + (p5 * p5);
+		len3=sqrt((p3 * p3) + (p6 * p6);
+		smallest(len1,len2,len3,&smallest1,&smallest2);
+		res[i] = (*smallest1) * (*smallest2);
+	}
 }
+void smallest(float len1,float len2,float len3,*smallest1,*smallest2)
+{
+	if(len1>len2 && len1>len3)
+	{
+		*smallest1=len2;
+		*smallest2=len3;
+	}
+	else if(len2>len1 && len2>len3)
+	{
+		*smallest1=len1;
+		*smallest2=len3;
+	}
+	else 
+	{
+		*smallest1=len1;
+		*smallest2=len2;
+	}
+}
+
 void output(int n,float area[])
 {
 	printf("the area of the %d rectangles are:\n",n);
