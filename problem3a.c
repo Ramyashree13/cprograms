fa1634658354779c61e4dec0c549798220fc49f6
unknown <ramya>
2019-07-05 11:28:39 +0530

    output function is remaining.

--- problem3a.c
+++ problem3a.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void palin(int begin,int end,char s[]);
+void palindrome(int begin,int end,char s[]);
+//int memoryalloc(int m,int n,char *resultstr);
 struct str
 {
 	int len;
@@ -18,15 +19,11 @@ void input(int n,struct str string[n])
 		printf("enter the string\n");
 		scanf("%s",string[i].s);
 	}
-//	for(int i=0;i<n;i++)
-		//for(int j=0;j<string[i].len;j++)
-			//printf("%s",string[i].s);
-
-
 }
 void compute(int n,struct str string[n])
 {
 	int p,begin,end;
+//	char *resultstr;
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<string[i].len;j++)
@@ -36,15 +33,14 @@ void compute(int n,struct str string[n])
 			{
 				begin=p;
 				end=k;
-				palin(begin,end,string[i].s);
-				printf("in function %d\t%d\n",j,k);
+				palindrome(begin,end,string[i].s);
 			}
 		}
 	}
 }
-void palin(int begin,int end,char s[])
+void palindrome(int begin,int end,char s[])
 {
-	char d[10],a[10];
+	char d[10],a[10],res[20];
 	int c=0,i,n;
 	for( i=begin;i<=end;i++)
 	{
@@ -53,23 +49,33 @@ void palin(int begin,int end,char s[])
 	}
 	d[c]=NULL;
 	n=strlen(d);
-	//printf("d=%s\n",d);
-	printf("length=%d",n);
 	for(int i=0;i<n;i++)
 	{
 		a[(n-1)-i]=d[i];
 	}
 	a[n]=NULL;
-	printf("palin= %s\n",a);
 	if(strcmp(a,d)==0)
 	{
 		if(strlen(a)>=3)
 		{
-			printf("\npalindrome= %s\n",a);
+			/*memoryalloc(strlen(s),strlen(a),resultstr);
+			strcpy(*resultstr,a);
+			printf("%s",*resultstr);*/
+			printf("%d\t",begin+1);
+			strcpy(res,a);
+			printf("%s\n",res);
+
 		}
 	}
 }
+/*int memoryalloc(int m,int n,char *resultstr)
+{
 
+	resultstr=(char *)malloc(m * sizeof(char));
+	for(int i=0;i<m;i++)
+		resultstr=(char *) malloc(n * sizeof(char));
+	return *resultstr;
+}*/
 int main()
 {
 	int n;
