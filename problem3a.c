4d1da1828d101d81a5b4e16a418435f35d809f9e
unknown <ramya>
2019-07-05 10:41:27 +0530

    completed logic.

--- problem3a.c
+++ problem3a.c
@@ -18,9 +18,9 @@ void input(int n,struct str string[n])
 		printf("enter the string\n");
 		scanf("%s",string[i].s);
 	}
-	for(int i=0;i<n;i++)
+//	for(int i=0;i<n;i++)
 		//for(int j=0;j<string[i].len;j++)
-			printf("%s",string[i].s);
+			//printf("%s",string[i].s);
 
 
 }
@@ -32,33 +32,42 @@ void compute(int n,struct str string[n])
 		for(int j=0;j<string[i].len;j++)
 		{
 			p=j;
-			for(int k=1;k<string[i].len;k++)
+			for(int k=j+1;k<(string[i].len);k++)
 			{
 				begin=p;
 				end=k;
 				palin(begin,end,string[i].s);
+				printf("in function %d\t%d\n",j,k);
 			}
 		}
 	}
 }
 void palin(int begin,int end,char s[])
 {
-	char d[10],a[10],n;
-	int c=0;
-	for(int i=begin;i<end;i++)
+	char d[10],a[10];
+	int c=0,i,n;
+	for( i=begin;i<=end;i++)
 	{
 		d[c]=s[i];
 		c++;
 	}
-	n=strlen(s);
+	d[c]=NULL;
+	n=strlen(d);
+	//printf("d=%s\n",d);
+	printf("length=%d",n);
 	for(int i=0;i<n;i++)
 	{
-		a[n-i]=s[i];
+		a[(n-1)-i]=d[i];
 	}
+	a[n]=NULL;
+	printf("palin= %s\n",a);
 	if(strcmp(a,d)==0)
-		printf("palindrome");
-	
-	printf("%s\n",d);
+	{
+		if(strlen(a)>=3)
+		{
+			printf("\npalindrome= %s\n",a);
+		}
+	}
 }
 
 int main()
