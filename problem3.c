90f7cfbf10b437867827a9ef4870a89dab7c0858
unknown <ramya>
2019-07-09 08:22:21 +0530

    using structure member to store the strings.

--- problem3.c
+++ problem3.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int palindrome(int begin,int end,char d[]);
+int palindrome(int begin,int end,char d[],struct str string,int n,char *resstr);
 struct str
 {
 	int len;
 	char *s;
+	int pos;
+	int a[10];
 };
 void input(int n,struct str string[n])
 {
@@ -21,8 +23,8 @@ void input(int n,struct str string[n])
 }
 void compute(int n,struct str string[n])
 {
-	int p,begin,end,c=0;
-	char d[10];
+	int p,begin,end,c=0,m;
+	char d[10],*resstr[10];
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<string[i].len;j++)
@@ -32,24 +34,24 @@ void compute(int n,struct str string[n])
 			{
 				begin=p;
 				end=k;
-				for( i=begin;i<=end;i++)
+				for( m=begin;m<=end;m++)
 				{
-					d[c]=string[i].s[i];
+					d[c]=string[i].s[m];
 					c++;
 				}
 				d[c]=NULL;
-				printf("%s",d);
-				palindrome(begin,end,d);
-				printf("%d\n",k);
+				palindrome(begin,end,d,string,n,resstr);
+				printf("%s",resstr);
+				c=0;
+				d[c]='\0';
 			}
 		}
 	}
 }
-int palindrome(int begin,int end,char d[])
+int palindrome(int begin,int end,char d[],struct str string,int n,char *resstr)
 {
 	char a[10],res[20];
 	int i,n;
-	printf("substring\n");
 	/*for( i=begin;i<=end;i++)
 	{
 		d[c]=s[i];
@@ -57,7 +59,6 @@ int palindrome(int begin,int end,char d[])
 	}
 	d[c]=NULL;*/
 	n=strlen(d);
-	printf("%d\n",n);
 	for(int i=0;i<n;i++)
 	{
 		a[(n-1)-i]=d[i];
@@ -68,13 +69,18 @@ int palindrome(int begin,int end,char d[])
 		if(strlen(a)>=3)
 		{
 			printf("%d\t",begin+1);
-			strcpy(res,a);
-			printf("%s\n",res);
+			strcpy(resstr,a);
+			printf("%s\n",resstr);
+			return resstr;
 
 		}
 	}
 	return 0;
 }
+/*void output()
+{
+	printf("%d\t %s\n",)
+}*/
 int main()
 {
 	int n;
